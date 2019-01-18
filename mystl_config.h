13accfedc5c5289f1deaad0f3d069ae7b730d4be
Brang Pakdring <Brang.Pakdring@gmail.com>
2019-01-19 00:24:01 +0800

    mystl global config

--- mystl_config.h
+++ mystl_config.h
@@ -5,4 +5,15 @@
 #ifndef MYSTL_MYSTL_CONFIG_H
 #define MYSTL_MYSTL_CONFIG_H
 
+#include <stdlib.h>
+using std::size_t;
+using std::ptrdiff_t;
+
+namespace mystd{}
+#ifndef NAMESPACE_MYSTD
+#define NAMESPACE_MYSTD mystd
+#define BEGIN_NAMESPACE_MYSTD namespace mystd {
+#define END_NAMESPACE_MYSTD }
+#endif
+
 #endif //MYSTL_MYSTL_CONFIG_H
