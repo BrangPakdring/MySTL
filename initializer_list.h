c55649def826dba6ed59ce33d2cb9f510f099cf6
Brang Pakdring <Brang.Pakdring@gmail.com>
2019-01-22 14:46:01 +0800

    stack impl

--- initializer_list.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by brang on 1/22/19.
-//
-
-#ifndef MYSTL_INITIALIZER_LIST_H
-#define MYSTL_INITIALIZER_LIST_H
-
-#endif //MYSTL_INITIALIZER_LIST_H
--- main.cpp
+++ main.cpp
@@ -9,12 +9,5 @@ using std::endl;
 using namespace mystd;
 int main()
 {
-	std::list<int>ll = {1,2,3,4};
-	ll.begin()._M_node->_M_next = nullptr;
-	cout << ll.size() << endl;
-	list<int>l;
-	l.push_front(1);
-	l.push_back(2);
-	for (auto it = l.cbegin(); it != l.cend(); ++it)
-		cout << (*it) << endl;
+
 }
